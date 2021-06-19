15df78e9f874d3f1bdfcb40c9241065e54417eba
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-06-18 23:56:42 -0400

    speech in Bank 7

--- Source/Banks/Bank07/Bank07.s
+++ Source/Banks/Bank07/Bank07.s
@@ -3,14 +3,21 @@
           ;;
           ;; Sound and Music services
           ;;
-          ;; (not speech)
-          ;; 
 
           .include "StartBank.s"
 
 DoLocal:
+
           .include "PlaySFX.s"
+          .include "PlaySpeech.s"
 
           .include "SoundEffects.s"
-          
+
+MovesSpeech:        
+          .include "MovesSpeech.s"
+GrizzardsSpeech:    
+          .include "GrizzardsSpeech.s"
+CombatSpeech:
+          .include "CombatSpeech.s"
+
           .include "EndBank.s"
