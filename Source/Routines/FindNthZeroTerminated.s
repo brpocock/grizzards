cb05bf1cd1ad83958ee38bb2319c40c56993745b
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-06-19 09:30:53 -0400

    new routine

---
+++ Source/Routines/FindNthZeroTerminated.s
@@ -0,0 +1,30 @@
+FindNthZeroTerminated:        .block
+
+          ;; Finds the nth (xth) zero-terminated string starting after Pointer.
+          ;; Updates Pointer to point to the head of that string.
+          ;; Each string must be ≤ 255 bytes in length,
+          ;; but they can total much more.
+
+          cpx # 0
+          beq Done
+          ldy # 0
+SearchMore:         
+          lda (Pointer),y
+          beq StringEnd
+          iny
+          bne SearchMore
+
+StringEnd:
+          tya
+          clc
+          adc Pointer
+          bcc +
+          inc Pointer + 1
++
+          dex
+          bne SearchMode
+
+Done:     
+
+          rts
+          .bend
