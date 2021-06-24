72ae97013a27a37523a0d1b78cfbcd2b9b1d9293
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-06-24 10:18:04 -0400

    add sprite for wild Grizzard

--- Source/Common/MapSprites.s
+++ Source/Common/MapSprites.s
@@ -61,8 +61,28 @@ GrizzardDepotSprite:
           .byte %11111111
           .byte %11111111
 
+GrizzardSprite:
+          .byte %11000000
+          .byte %11100000
+          .byte %11101010
+          .byte %01110001
+          .byte %00111101
+          .byte %00011101
+          .byte %00001111
+          .byte %00000110
+
+          .byte %10100000
+          .byte %11100000
+          .byte %11110001
+          .byte %01110101
+          .byte %00111001
+          .byte %00011101
+          .byte %00001111
+          .byte %00000110
+
+
 SpriteColor:
           .colu COLRED, 8
           .colu COLGREEN, 8
           .colu COLINDIGO, $f
-
+          .colu COLSPRINGGREEN, 8
