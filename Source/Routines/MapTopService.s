0cdef4cf8080d72835c6e097479c0397bc82f6d3
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-06-24 16:01:14 -0400

    fix score display

--- Source/Routines/MapTopService.s
+++ Source/Routines/MapTopService.s
@@ -20,62 +20,38 @@ DecodeScore:
 
           lda Score             ; rightmost digit
           and #$0f
-          sta Temp
-          rol a
-          rol a
-          clc
-          adc Temp
           sta StringBuffer + 5
 
           lda Score
           and #$f0
           ror a
           ror a
-          sta Temp
           ror a
           ror a
-          clc
-          adc Temp
           sta StringBuffer + 4
 
           lda Score + 1
           and #$0f
-          sta Temp
-          rol a
-          rol a
-          clc
-          adc Temp
           sta StringBuffer + 3
 
           lda Score + 1
           and #$f0
           ror a
           ror a
-          sta Temp
           ror a
           ror a
-          clc
-          adc Temp
           sta StringBuffer + 2
 
           lda Score + 2
           and #$0f
-          sta Temp
-          rol a
-          rol a
-          clc
-          adc Temp
           sta StringBuffer + 1
 
           lda Score + 2         ; leftmost digit
           and #$f0
           ror a
           ror a
-          sta Temp
           ror a
           ror a
-          clc
-          adc Temp
           sta StringBuffer + 0
 
 ScoreDone:          
