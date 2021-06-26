16d5c918504c6759be1051bc3f9f7ddd21d73575
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-06-26 14:01:09 -0400

    new module

---
+++ Source/Routines/SaveProvinceData.s
@@ -0,0 +1,33 @@
+;;; Grizzards Source/Routines/SaveProvinceData.s
+;;; Copyright © 2021 Bruce-Robert Pocock
+
+SaveProvinceData:   .block
+
+          PositionProvinceData:
+          jsr i2cStartWrite
+          
+	lda #>SaveGameSlotPrefix
+	clc
+	adc SaveGameSlot
+	jsr i2cTxByte
+
+          lda CurrentProvince
+          asl a
+          asl a
+          asl a                 ; ×8
+          ora # $20
+          jsr i2cTxByte
+
+WriteProvinceData:
+          ldx # 0
+-
+          lda ProvinceFlags, x
+          lda # $ed             ; FIXME for testing only
+          jsr i2cTxByte
+          inx
+          cpx # 8             ; 4 provinces × 8 bytes of flags each
+          bne -
+
+          jmp i2cStopWrite      ; tail call
+
+          .bend
