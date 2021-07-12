838fe3add85830dba5721ac8163912224e31872f
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-07-12 14:52:11 -0400

    GAME OVER screen

--- Source/Routines/Death.s
+++ Source/Routines/Death.s
@@ -7,11 +7,37 @@ Death:    .block
           ldx #$ff
           txs
 
-          lda #$ad
-          pha
-          lda #$de
-          pha
+          ldx #ModeDeath
+          stx GameMode
+
+Loop:
+          jsr VSync
+          jsr VBlank
           
-          brk                   ; TODO
+          jsr Prepare48pxMobBlob
+          .LoadString " GAME "
+          ldx #TextBank
+          ldy #ServiceDecodeAndShowText
+          jsr FarCall
+          .LoadString " OVER "
+          ldx #TextBank
+          ldy #ServiceDecodeAndShowText
+          jsr FarCall
+
+          ldx # KernelLines - 40
+-
+          stx WSYNC
+          dex
+          bne -
+
+          jsr Overscan
+
+          lda GameMode
+          cmp #ModeDeath
+          bne Leave
+          jmp Loop
+
+Leave:
+          jmp GoColdStart
 
           .bend
