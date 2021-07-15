23d520cf8b9dd57bdb6e99dd853c63da053f45a8
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-07-15 16:55:05 -0400

    use .colu macros so PAL, SECAM can keep up

--- Source/Banks/Bank06/Monsters6.s
+++ Source/Banks/Bank06/Monsters6.s
@@ -4,62 +4,62 @@ Monsters:
           .MiniText "WICKED"
           .MiniText "SLIME "
           .byte 14               ; art
-          .byte COLGREEN | $f
+          .colu COLGREEN, $f
           .byte $10, 6         ; ATK/DEF, + score to defeat
           
           .MiniText "HORRID"
           .MiniText "SLIME "
           .byte 14               ; art
-          .byte COLRED | $f
+          .colu COLRED, $f
           .byte $20, 6        ; ATK/DEF, score
           
 
           .MiniText "VORPAL"
           .MiniText "BUNNY "
           .byte 0
-          .byte COLGOLD | $4
+          .colu COLGOLD, $4
           .byte $22, 6
           
           .MiniText "R.O.  "
           .MiniText "  U.S."
           .byte 1
-          .byte COLGOLD | $0
+          .colu COLGOLD, $0
           .byte $31, 15
 
 	.MiniText "LECTRO"
 	.MiniText " SHEEP"
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 15
 
 	.MiniText "VIKING"
 	.MiniText "TURTLE"
 	.byte 3
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 15
 
 	.MiniText "CRAZY "
 	.MiniText "   FOX"
 	.byte 4
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 25
 
 	.MiniText "WATER "
 	.MiniText " KITTY"
 	.byte 5
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 20
 
 	.MiniText "FLAME "
 	.MiniText " DOGGO"
 	.byte 6
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 20
 
 	.MiniText "FUZZIE"
 	.MiniText "  BEAR"
 	.byte 7
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 ;;; 10
@@ -67,239 +67,239 @@ Monsters:
 	.MiniText "METAL "
 	.MiniText " MOUSE"
 	.byte 8
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "FIRE  "
 	.MiniText " PANDA"
 	.byte 9
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "LEGGY "
 	.MiniText "MUTANT"
 	.byte 10
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "SKY   "
 	.MiniText "MUTANT"
 	.byte 11
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "WILL-O"
 	.MiniText " -WISP"
 	.byte 12
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "BUTTER"
 	.MiniText "   FLY"
 	.byte 13
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "SCARY "
 	.MiniText "   RAT"
 	.byte 1
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  17  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  18  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  19  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  20  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  21  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  22  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  23  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  24  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  25  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  26  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  27  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  28  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  29  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  30  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  31  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  32  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  33  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  34  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  35  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  36  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  37  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  38  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  39  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  40  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  41  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  42  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  43  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  44  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  45  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  46  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  47  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  48  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
 
 	.MiniText "MONSTR"
 	.MiniText "  49  "
 	.byte 2
-	.byte COLBLUE | $8
+	.colu COLBLUE, $8
 	.byte $00, 10
