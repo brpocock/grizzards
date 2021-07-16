f1731599ed662e6f14e7e7c45a5d8edb6069b48b
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-07-16 12:55:35 -0400

    update move effects/Δhp values

--- Source/Common/MoveEffects.s
+++ Source/Common/MoveEffects.s
@@ -7,21 +7,21 @@ MoveEffects:
           .byte 0
           .byte 0
           .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
+          .byte StatusSleep
+          .byte StatusDefendDown
+          .byte StatusAttackDown
           ;; 8
+          .byte StatusDefendDown
+          .byte StatusDefendUp
+          .byte StatusAttackUp
+          .byte StatusDefendUp
+          .byte StatusAttackUp
           .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
+          .byte StatusAttackDown
+          .byte StatusDefendDown
           ;; 16
-          .byte 0
-          .byte 0
+          .byte StatusSleep
+          .byte StatusAttackUp
           .byte 0
           .byte 0
           .byte 0
@@ -31,9 +31,9 @@ MoveEffects:
           ;; 24
           .byte 0
           .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
+          .byte StatusAttackDown | StatusDefendDown
+          .byte StatusAttackDown
+          .byte StatusDefendDown
           .byte 0
           .byte 0
           .byte 0
@@ -42,7 +42,7 @@ MoveEffects:
           .byte 0
           .byte 0
           .byte 0
-          .byte 0
+          .byte StatusMuddle
           .byte 0
           .byte 0
           .byte 0
@@ -78,44 +78,44 @@ MoveEffects:
 MoveDeltaHP:
           ;; 0
           .byte $ff             ; RUN AWAY has fake value to indicate there's no target
-          .byte 4
-          .byte 1
-          .byte 1
-          .byte 1
-          .byte 1
-          .byte 1
-          .byte 1
+          .byte 2
+          .byte 2
+          .byte 2
+          .byte 2
+          .byte 0
+          .byte 5
+          .byte 0
           ;; 8
-          .byte 1
           .byte 0
           .byte 0
           .byte 0
-          .byte 0
+          .byte 5
+          .byte 5
           .byte 0
           .byte 0
           .byte 0
           ;; 16
           .byte 0
           .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
+          .byte 5
+          .byte 10
+          .byte 15
+          .byte 5
+          .byte 10
+          .byte 15
           ;; 24
+          .byte 25
+          .byte 25
           .byte 0
           .byte 0
           .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
-          .byte 0
+          .byte ~2
+          .byte ~5
+          .byte ~10
           ;; 32
-          .byte 0
-          .byte 0
-          .byte 0
+          .byte ~25
+          .byte ~50
+          .byte ~99
           .byte 0
           .byte 0
           .byte 0
