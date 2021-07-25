9828d2aaf8175076fd6010d54611935819c0f319
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-07-25 18:08:37 -0400

    don't call .SkipLines macro from Skip11Lines subroutine
    The macro is now “smart” and will expand to a call to that subroutine

--- Source/Common/Skip11Lines.s
+++ Source/Common/Skip11Lines.s
@@ -3,8 +3,15 @@
 
 ;;; Saves a few bytes of ROM in PAL/SECAM .WaitScreenBottom macroexpansion.
 ;;; Not needed for NTSC's shorter screen height.
+
+;;; Don't  use the  .SkipLines macro  here, as  it is  “smart” and  will
+;;; expand to a call to this subroutine automatically.
           .if TV != NTSC
 Skip11Lines:
-          .SkipLines 11
+          ldx # 11
+-
+          stx WSYNC
+          dex
+          bne -
           rts
           .fi
