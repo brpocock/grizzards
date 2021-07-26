edfb0984f352518a5594aba0b5cc2f9980251803
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-07-26 10:55:07 -0400

    check collisions before movement code
    zeroing the DeltaX/DeltaY vars was causing the "stick to walls" bug
    
    close #77
    ref: #39 was fixed improperly/incompletely before now

--- Source/Routines/MapVBlank.s
+++ Source/Routines/MapVBlank.s
@@ -209,6 +209,140 @@ SpriteCxDown:
           ;; fall through
 MovementLogicDone:
 ;;; 
+;;; Collision Handling
+CheckPlayerMove:
+          lda CXP0FB
+          and #$c0              ; hit playfield or ball
+          beq NoBumpWall
+          jsr BumpWall
+          jmp DonePlayerMove
+
+NoBumpWall:
+          lda CXPPMM
+          .BitBit $80              ; hit other sprite
+          beq PlayerMoveOK
+
+BumpSprite:
+          jsr BumpWall
+
+          lda BumpCooldown
+          bne PlayerMoveOK
+          
+          ldx SpriteFlicker
+          lda SpriteAction, x
+          cmp #SpriteDoor
+          beq DoorWithSprite
+          cmp #SpriteCombat
+          beq FightWithSprite
+          cmp #SpriteGrizzardDepot
+          beq EnterDepot
+          cmp #SpriteGrizzard
+          beq GetNewGrizzard
+          and #SpriteProvinceDoor
+          beq PlayerMoveOK      ; No action
+          jmp ProvinceChange
+
+FightWithSprite:
+          ldx SpriteFlicker     ; ? Seems unnecessary
+FightWithSpriteX:
+          lda SpriteParam, x
+          sta CurrentCombatEncounter
+          lda SpriteIndex, x
+          sta CurrentCombatIndex
+          lda #ModeCombat
+          sta GameMode
+          rts
+
+DoorWithSprite:
+          lda SpriteParam, x
+          sta NextMap
+          ldy #ModeMapNewRoom
+          sty GameMode
+          .WaitForTimer
+          rts
+
+GetNewGrizzard:
+          lda #ModeNewGrizzard
+          sta GameMode
+          rts
+
+PlayerMoveOK:
+          lda #0
+          lda PlayerX
+          sta BlessedX
+          lda PlayerY
+          sta BlessedY
+
+DonePlayerMove:
+          ldy #$00
+          beq UserInputStart
+
+EnterDepot:
+          lda #ModeGrizzardDepot
+          sta GameMode
+          rts
+
+ProvinceChange:
+          lda SpriteAction, x
+          and #$70
+          clc
+          ror a
+          ror a
+          ror a
+          ror a
+          sta CurrentProvince
+          lda SpriteParam, x
+          sta NextMap
+          jsr Overscan
+          ldy #ModeMapNewRoom
+          sty GameMode
+          rts
+;;; 
+BumpWall:
+          sta CXCLR
+
+          lda BlessedX
+          cmp PlayerX
+          beq +
+          sta PlayerX
+          jmp BumpY
++
+          lda DeltaX
+          bne ShoveX
+          jsr Random
+          and # 1
+          bne ShoveX
+          lda #-1
+ShoveX:
+          sta DeltaX
+          clc
+          adc PlayerX
+          sta PlayerX
+
+BumpY:    
+          lda BlessedY
+          cmp PlayerY
+          beq +
+          sta PlayerY
+          jmp DoneBump
++
+          lda DeltaY
+          bne ShoveY
+          jsr Random
+          and # 1
+          bne ShoveY
+          lda #-1
+ShoveY:
+          sta DeltaY
+          clc
+          adc PlayerY
+          sta PlayerY
+
+DoneBump:           
+          lda #SoundBump
+          sta NextSound
+
+;;; 
 UserInputStart: 
           lda BumpCooldown
           beq HandleStick
@@ -297,139 +431,6 @@ DoneMovement:
 
           ;; fall through …
 ;;; 
-;;; Collision Handling
-CheckPlayerMove:
-          lda CXP0FB
-          and #$c0              ; hit playfield or ball
-          beq NoBumpWall
-          jsr BumpWall
-          jmp DonePlayerMove
-
-NoBumpWall:
-          lda CXPPMM
-          .BitBit $80              ; hit other sprite
-          beq PlayerMoveOK
-
-BumpSprite:
-          jsr BumpWall
-
-          lda BumpCooldown
-          bne PlayerMoveOK
-          
-          ldx SpriteFlicker
-          lda SpriteAction, x
-          cmp #SpriteDoor
-          beq DoorWithSprite
-          cmp #SpriteCombat
-          beq FightWithSprite
-          cmp #SpriteGrizzardDepot
-          beq EnterDepot
-          cmp #SpriteGrizzard
-          beq GetNewGrizzard
-          and #SpriteProvinceDoor
-          beq PlayerMoveOK      ; No action
-          jmp ProvinceChange
-
-FightWithSprite:
-          ldx SpriteFlicker     ; ? Seems unnecessary
-FightWithSpriteX:
-          lda SpriteParam, x
-          sta CurrentCombatEncounter
-          lda SpriteIndex, x
-          sta CurrentCombatIndex
-          lda #ModeCombat
-          sta GameMode
-          rts
-
-DoorWithSprite:
-          lda SpriteParam, x
-          sta NextMap
-          ldy #ModeMapNewRoom
-          sty GameMode
-          .WaitForTimer
-          rts
-
-GetNewGrizzard:
-          lda #ModeNewGrizzard
-          sta GameMode
-          rts
-
-PlayerMoveOK:
-          lda #0
-          lda PlayerX
-          sta BlessedX
-          lda PlayerY
-          sta BlessedY
-
-DonePlayerMove:
-          ldy #$00
-          rts
-
-EnterDepot:
-          lda #ModeGrizzardDepot
-          sta GameMode
-          rts
-
-ProvinceChange:
-          lda SpriteAction, x
-          and #$70
-          clc
-          ror a
-          ror a
-          ror a
-          ror a
-          sta CurrentProvince
-          lda SpriteParam, x
-          sta NextMap
-          jsr Overscan
-          ldy #ModeMapNewRoom
-          sty GameMode
-          rts
-;;; 
-BumpWall:
-          sta CXCLR
-
-          lda BlessedX
-          cmp PlayerX
-          beq +
-          sta PlayerX
-          jmp BumpY
-+
-          lda DeltaX
-          bne ShoveX
-          jsr Random
-          and # 1
-          bne ShoveX
-          lda #-1
-ShoveX:
-          sta DeltaX
-          clc
-          adc PlayerX
-          sta PlayerX
-
-BumpY:    
-          lda BlessedY
-          cmp PlayerY
-          beq +
-          sta PlayerY
-          jmp DoneBump
-+
-          lda DeltaY
-          bne ShoveY
-          jsr Random
-          and # 1
-          bne ShoveY
-          lda #-1
-ShoveY:
-          sta DeltaY
-          clc
-          adc PlayerY
-          sta PlayerY
-
-DoneBump:           
-          lda #SoundBump
-          sta NextSound
-
           rts
 ;;; 
           .bend
