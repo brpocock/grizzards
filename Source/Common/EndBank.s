27432089015c998d1fdc6fbba483abdb996e3d06
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-08-13 18:43:19 -0400

    bank end addresses

--- Source/Common/EndBank.s
+++ Source/Common/EndBank.s
@@ -2,7 +2,11 @@
 
 EndBank:
 
+          .if DEMO
+          BankEndAddress = $ff48      ; keep this as high as possible
+          .else
           BankEndAddress = $ff34      ; keep this as high as possible
+          .fi
           ;; The magic number  above: you can just raise it  to, say,
           ;; $ff70,  and then  the assembler  will bitch  at you  about its
           ;; being too high, and tell you  what to lower it to. Careful,
