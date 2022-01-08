f8532cffc7677b98d57aeaf10760a7917a11060a
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-08 00:11:32 -0500

    starting stats (and names) for all Grizzards

--- Source/Banks/Bank01/GrizzardStartingStats.s
+++ Source/Banks/Bank01/GrizzardStartingStats.s
@@ -6,38 +6,71 @@ GrizzardStartingStats:        .block
           ;; Max HP, ATK, DEF, (unused), and MovesKnown
 
           ;; 0
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 12, 4, 4, 4, $0f
-          .byte 12, 4, 4, 4, $0f
-          .byte 12, 4, 4, 4, $0f
-          .byte 12, 4, 4, 4, $0f
-          .byte 14, 6, 6, 6, $0f
-          .byte 14, 6, 6, 6, $0f
-          .byte 14, 6, 6, 6, $0f
+Dirtex:
+          .byte  8,  1,  1,  0,  $0f
+Aquax:
+          .byte  8,  1,  1,  0,  $0f
+Airex:
+          .byte  8,  1,  1,  0,  $0f
+Flamex:
+          .byte 10,  4,  4,  0,  $0f
+Soiley:
+          .byte 10,  4,  4,  0,  $0f
+Wetnas:
+          .byte 10,  4,  4,  0,  $0f
+Windoo:
+          .byte 10,  4,  4,  0,  $0f
+Firend:
+          .byte 12,  6,  6,  0,  $0f
+          ;; 8
+Lander:
+          .byte 12,  6,  6,  0,  $0f
+Sailor:
+          .byte 12,  6,  6,  0,  $0f
           ;; 10
-          .byte 14, 6, 6, 6, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
+Flyer:
+          .byte 12,  6,  6,  0,  $0f
+Burner:
+          .byte 16,  8,  8,  0,  $0f
+Splodo:
+          .byte  5, 75, 15,  0,  $0f
+Tyrant:
+          .byte 20, 20, 20,  0,  $0f
+Dufont:
+          .byte 30, 15, 15,  0,  $0f
+Theref:
+          .byte 30, 15, 15,  0,  $0f
+          ;; 16
+Cornet:
+          .byte 30, 15, 15,  0,  $0f
+Ambren:
+          .byte 40, 20, 20,  0,  $0f
+Noctis:
+          .byte 40, 20, 20,  0,  $0f
+Corlyn:
+          .byte 40, 20, 20,  0,  $0f
           ;; 20
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
-          .byte 10, 1, 1, 1, $0f
+Wapow:
+          .byte 45, 24, 24,  0,  $0f
+Zendex:
+          .byte 45, 24, 24,  0,  $0f
+Oceax:
+          .byte 45, 24, 24,  0,  $0f
+Flitex:
+          .byte 50, 25, 25,  0,  $0f
+          ;; 24
+Flarex:
+          .byte 50, 25, 25,  0,  $0f
+Uptrix:
+          .byte 75, 37, 37,  0,  $0f
+Altrix:
+          .byte 75, 37, 37,  0,  $0f
+Ectrix:
+          .byte 75, 37, 37,  0,  $0f
+Ortrix:
+          .byte 75, 37, 37,  0,  $0f
+Megax:
+          .byte 90, 90, 90,  0,  $ff
           ;; ↑ 29
           
           .bend
--- Source/Banks/Bank02/GrizzardNames.s
+++ Source/Banks/Bank02/GrizzardNames.s
@@ -1,38 +1,72 @@
 ;;; Grizzards Source/Banks/Bank02/GrizzardNames.s
 ;;; Copyright © 2021-2022 Bruce-Robert Pocock
-GrizzardNames:
+GrizzardNames:      .block
           ;; 0
+Dirtex:
           .MiniText "DIRTEX"
+Aquax:
           .MiniText "AQUAX "
+Airex:
           .MiniText "AIREX "
+Flamex:
           .MiniText "FLAMEX"
+Soiley:
           .MiniText "SOILEY"
+Wetnas:
           .MiniText "WETNAS"
+Windoo:
           .MiniText "WINDOO"
+Firend:
           .MiniText "FIREND"
           ;; 8
+Lander:
           .MiniText "LANDER"
+Sailor:
           .MiniText "SAILOR"
+          ;; 10
+Flyer:
           .MiniText "FLYER "
+Burner:
           .MiniText "BURNER"
-          .MiniText "IGUANA"
+Splodo:
+          .MiniText "SPLODO"
+Tyrant:
           .MiniText "TYRANT"
+Dufont:
           .MiniText "DUFONT"
+Theref:
           .MiniText "THEREF"
           ;; 16
+Cornet:
           .MiniText "CORNET"
+Ambren:
           .MiniText "AMBREN"
+Noctis:
           .MiniText "NOCTIS"
+Corlyn:
           .MiniText "CORLYN"
+          ;; 20
+Wapow:
           .MiniText "WAPOW "
+Zendex:
           .MiniText "ZENDEX"
+Oceax:
           .MiniText "OCEAX "
+Flitex:
           .MiniText "FLITEX"
           ;; 24
+Flarex:
           .MiniText "FLAREX"
+Uptrix:
           .MiniText "UPTRIX"
+Altrix:
           .MiniText "ALTRIX"
+Ectrix:
           .MiniText "ECTRIX"
+Ortrix:
           .MiniText "ORTRIX"
+Megax:
           .MiniText "MEGAX "
           ;; ↑ 29
+
+          .bend
