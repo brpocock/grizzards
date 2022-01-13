8bce87359f648256f0ac56581a0ba7127872e9b5
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-12 21:17:00 -0500

    “compact” the speech into lower banks

--- Source/Banks/Bank08/SignpostText.s
+++ Source/Banks/Bank08/SignpostText.s
@@ -5,7 +5,7 @@
 
           FirstSignpost = 0
 
-          Signs = (Sign_Beware, Sign_FireSwamp, NPC_SouthGate, NPC_TunnelBlocked, NPC_TunnelOpen, NPC_Artifact, NPC_TakeArtifact1, NPC_TakeArtifact2, NPC_TookArtifact, Sign_LostMines, NPC_RandomVillager, Sign_TrebleVillage, NPC_TrebleVillage, Sign_TrebleDocks, Sign_WesternRoad)
+          Signs = ( Sign_Beware, Sign_FireSwamp, NPC_SouthGate, NPC_TunnelBlocked, NPC_TunnelOpen, NPC_Artifact, NPC_TakeArtifact1, NPC_TakeArtifact2, NPC_TookArtifact, Sign_LostMines, NPC_RandomVillager, Sign_TrebleVillage, NPC_TrebleVillage, Sign_TrebleDocks, Sign_WesternRoad, NPC_Artifact1Scared, NPC_BrokenRadio, Sign_TunnelClosed, Sign_SpiralWoodsOpen, Sign_PortLionShip, Sign_TunnelMazeBlocked, NPC_LostPendant, Random_FoundPendant, NPC_ReturnPendant )
           
 SignH:    .byte >(Signs)
 SignL:    .byte <(Signs)
@@ -165,4 +165,107 @@ Sign_WesternRoad:
           .SignText "      TUNNEL"
           .SignText "  COMPLEX ->"
           .byte ModeSignpostDone
+
+;;; 15
+NPC_Artifact1Scared:
+          .colu COLINDIGO, 0
+          .colu COLTURQUOISE, $9
+          .SignText "I HAVE THE  "
+          .SignText "ARTIFACT    "
+          .SignText "HIDDEN FROM "
+          .SignText "THE MONSTERS"
+          .SignText "SAVE ME!    "
+          .byte ModeSignpostDone
+
+;;; 16
+NPC_BrokenRadio:
+          .colu COLINDIGO, 0
+          .colu COLBLUE, $9
+          .byte $ff, 27, 59     ; radio fixed
+          .SignText "IF YOU FIX  "
+          .SignText "MY RADIO SET"
+          .SignText "I COULD CALL"
+          .SignText "FOR A SHIP  "
+          .SignText "TO PORT LION"
+          .byte ModeSignpostSetFlag, 25
+          
+;;; 17
+Sign_TunnelClosed:
+          .colu COLGRAY, 0
+          .colu COLBROWN, $8
+          .SignText "TUNNELS TO  "
+          .SignText "SOUTH FIELD "
+          .SignText "AND ANCHOR  "
+          .SignText "VILLAGE     "
+          .SignText "ARE CLOSED. "
+          .byte ModeSignpostDone
+
+;;; 18
+Sign_SpiralWoodsOpen:
+          .colu COLGRAY, 0
+          .colu COLBROWN, $6
+          .SignText "NOW ENTERING"
+          .SignText "SPIRAL WOODS"
+          .SignText "            "
+          .SignText "- DANGER ! -"
+          .SignText "            "
+          .byte ModeSignpostDone
+
+
+;;; 19
+Sign_PortLionShip:
+          .colu COLBLUE, $e
+          .colu COLCYAN, $2
+          .SignText "TREBLE DOCKS"
+          .SignText "NEXT SHIP TO"
+          .SignText "PORT LION   "
+          .SignText "DEPARTING.  "
+          .SignText "ALL ABOARD! "
+          .byte ModeSignpostWarp, 2, 0
+
+;;; 20
+Sign_TunnelMazeBlocked:
+          .colu COLINDIGO, $4
+          .colu COLBLUE, $e
+          .SignText "BLOCKADE IS "
+          .SignText "LOCKED DUE  "
+          .SignText "TO DANGER OF"
+          .SignText "A CAVE GRUE."
+          .SignText "NO ENTRY.   "
+          .byte ModeSignpostDone
+
+;;; 21
+NPC_LostPendant:
+          .colu COLINDIGO, 0
+          .colu COLTURQUOISE, $9
+          .byte $ff, 28, 22
+          .SignText "I TRAINED   "
+          .SignText "GRIZZARDS   "
+          .SignText "TOO. I LOST "
+          .SignText "A PENDANT   "
+          .SignText "IN THE WOODS"
+          .byte ModeSignpostDone
+
+;;; 22
+Random_FoundPendant:
+          .colu COLGRAY, $f
+          .colu COLGRAY, $0
+          .SignText "YOU HAVE    "
+          .SignText "FOUND A     "
+          .SignText "PENDANT ON  "
+          .SignText "A LEAF ON   "
+          .SignText "THE GROUND. "
+          .byte ModeSignpostSetFlag, 28
+
+;;; 23
+NPC_ReturnPendant:
+          .colu COLINDIGO, 0
+          .colu COLTURQUOISE, $9
+          .SignText "YOU'RE GREAT"
+          .SignText "THAT'S MY   "
+          .SignText "PENDANT. YOU"
+          .SignText "CAN HAVE    "
+          .SignText "THIS KEY.   "
+          .byte ModeSignpostClearFlag, 63
+
 
