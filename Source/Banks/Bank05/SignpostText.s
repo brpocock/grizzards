fc8e71d86af1b0e24afb7d5fa47b413352228bc4
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-19 00:11:17 -0500

    points for quest actions

--- Source/Banks/Bank05/SignpostText.s
+++ Source/Banks/Bank05/SignpostText.s
@@ -81,6 +81,8 @@ NPC_TakeArtifact1:
           .SignText "ARTIFACTS.  "
           .SignText "TAKE THIS TO"
           .SignText "THE TUNNELS."
+          .byte ModeSignpostPoints
+          .word $0020
           .byte ModeSignpostSetFlag, 18
 ;;; 7
 NPC_TakeArtifact2:
@@ -91,6 +93,8 @@ NPC_TakeArtifact2:
           .SignText "ARTIFACTS.  "
           .SignText "TAKE THIS TO"
           .SignText "THE TUNNELS."
+          .byte ModeSignpostPoints
+          .word $0025
           .byte ModeSignpostSetFlag, 17
 ;;; 8
 NPC_TookArtifact:
