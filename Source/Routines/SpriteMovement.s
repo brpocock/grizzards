350f0da876fa205492491ae73720b3a85de2fd45
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-22 17:43:47 -0500

    may have found where #307 needs to be fixed

--- Source/Routines/SpriteMovement.s
+++ Source/Routines/SpriteMovement.s
@@ -138,7 +138,7 @@ SpriteMoveDone:
           sta SpriteMotion, x
           inc SpriteX, x
 LeftOK:
-          cmp #ScreenRightEdge + 10
+          cmp #ScreenRightEdge + 10 ; XXX #307 this may be the culprit
           blt RightOK
           lda SpriteMotion, x
           eor # SpriteMoveLeft | SpriteMoveRight
