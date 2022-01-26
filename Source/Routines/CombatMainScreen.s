9d09a55338b01fbe4217849838592fa4924a64a3
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-26 15:12:31 -0500

    Fix alignment of monsters/player separator line
    Also fix every scanline error in PAL/SECAM versions and so forth
    
    Close #183.
    
    Also change sound effect to “Blip” on making a selection.
    
    Close #317.

--- Source/Routines/CombatMainScreen.s
+++ Source/Routines/CombatMainScreen.s
@@ -41,9 +41,9 @@ Loop:
           .if TV != NTSC
           lda WhoseTurn
           bne +
-          .SkipLines 3
+          .SkipLines 5
 +
-          .SkipLines 2
+          .SkipLines 0
           .fi
 LoopFirst:
           .WaitScreenTopMinus 1, 3
@@ -57,13 +57,13 @@ LoopFirst:
             .ldacolu COLRED, 0
             jmp BGTop
 PlayerTurnBGTop:
-            .ldacolu COLGRAY, $2
+            .ldacolu COLGRAY, $8
 
           .case PAL
 
             lda WhoseTurn
             beq PlayerTurnBGTop
-            .ldacolu COLRED, $2
+            .ldacolu COLRED, $8
             jmp BGTop
 PlayerTurnBGTop:
             .ldacolu COLGRAY, $2
@@ -73,7 +73,7 @@ PlayerTurnBGTop:
             ;; black background normally, but red for Boss Bear
             lda #COLBLACK
             ldx CurrentCombatEncounter
-            cpx # 92
+            cpx # 92            ; Boss Bear encounter
             bne BGTop
             lda #COLRED
 
@@ -278,7 +278,7 @@ DoUseMove:
 MoveOK:
           lda #ModeCombat
           sta GameMode
-          lda #SoundChirp
+          lda #SoundBlip
           sta NextSound
           gne CombatAnnouncementScreen
 
@@ -293,10 +293,16 @@ RunAway:
           .fi
           ;; gne RunningAway
 ;;; 
-ScreenDone:
 RunningAway:
-          .if TV == NTSC
+          .if SECAM == TV
+          .SkipLines 1
+          .fi
+          .if PAL == TV
           .SkipLines 3
+          .fi
+ScreenDone:
+          .if NTSC == TV
+          .SkipLines 4
           .fi
 
           lda GameMode
@@ -318,6 +324,9 @@ Leave:
           lda #ModeCombat
           sta DeltaY
           .WaitScreenBottom
+          .if NTSC != TV
+          .SkipLines 5
+          .fi
           jmp GrizzardStatsScreen
 +
           cmp #ModeCombatAnnouncement
