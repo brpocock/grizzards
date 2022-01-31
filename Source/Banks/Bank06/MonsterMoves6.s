4f14cb69e758edb720c13f014ab3380c6d0c5153
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-31 00:15:13 -0500

    monster moves

--- Source/Banks/Bank06/MonsterMoves6.s
+++ Source/Banks/Bank06/MonsterMoves6.s
@@ -46,7 +46,7 @@ MonsterMoves:       .block
           .byte 1, 2, 3, 4      ; Poison Asp
           .byte 1, 2, 3, 4      ; Grabby Crabby
           .byte 45, 48, 23, 30  ; Giant Bat
-          .byte 1, 2, 3, 4      ; Maze Jaguar
+          .byte 22, 23, 44, 32  ; Maze Jaguar
           .byte 1, 2, 3, 4
           ;; 40
           .byte 1, 2, 3, 4
