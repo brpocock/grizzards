28df61393ef98b295f45cc8662b11916a31ae2e3
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-01-30 23:47:14 -0500

    reduce 1 byte to fit in PAL ROM

--- Source/Banks/Bank01/Bank01.s
+++ Source/Banks/Bank01/Bank01.s
@@ -113,7 +113,7 @@ SkipStick:
           
 JatibuFF: .block
           ldx SelectJatibuProgress
-          cpx #$ff
+          inx                   ; text for $ff by rolling over to $00
           bne NotCompleted
 
           lda # 99
