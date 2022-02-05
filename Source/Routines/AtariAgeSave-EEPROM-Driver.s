3f555a8695583b42dbe910e158535cba981de501
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-05 18:02:28 -0500

    make room :(

--- Source/Routines/AtariAgeSave-EEPROM-Driver.s
+++ Source/Routines/AtariAgeSave-EEPROM-Driver.s
@@ -13,7 +13,7 @@ SaveGameSignatureString:
           ;; The AtariVox EEPROM driver is about 192 bytes ($c0)
           ;; This fill maxxes out bank 0,1 for AA Airex PAL/SECAM
           ;; if we need more room things gonna get ugly 😎
-          .fill $b0, $ea
+          .fill $ac, $ea
 
 i2cStartRead:
           ;; TODO
