26810df537c1420d15d618c3ef2233e0383fd187
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-06 17:52:32 -0500

    note to self

--- Source/Routines/AppendDecimalAndPrint.s
+++ Source/Routines/AppendDecimalAndPrint.s
@@ -7,6 +7,9 @@ FromTemp:
 
           ;; based on  https://stackoverflow.com/questions/65432063/6502-assembly-binary-to-bcd-is-that-possible-on-x86
 
+
+          ;; Not to self, consider also https://atariage.com/forums/topic/330847-binary-to-bcd/?do=findComment&comment=4999801 ?
+
 BINBCD8:
           sed
           lda Temp
