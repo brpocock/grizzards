6251eb65f438548b185785c6c119e7ebd90784c2
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-06 00:30:05 -0500

    punctuation for abbreviations

--- Source/Banks/Bank02/StringsTable.s
+++ Source/Banks/Bank02/StringsTable.s
@@ -3,15 +3,15 @@
 SleepText:
           .MiniText "SLEEP "    ; sleep
 AttackDownText:
-          .MiniText "ATK DN"    ; attack down
+          .MiniText "ATK.DN"    ; attack down
 DefendDownText:
-          .MiniText "DEF DN"    ; defend down
+          .MiniText "DEF.DN"    ; defend down
 MuddleText:
           .MiniText "MUDDLE"    ; muddle mind
 AttackUpText:
-          .MiniText "ATK UP"    ; attack up
+          .MiniText "ATK.UP"    ; attack up
 DefendUpText:
-          .MiniText "DEF UP"    ; defend up
+          .MiniText "DEF.UP"    ; defend up
 HPLostText:
           .MiniText "HP -00"
 HealedText:
