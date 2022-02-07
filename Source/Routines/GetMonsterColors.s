4b243832a6c704d316984ea37b07b6caa629929d
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-07 00:01:45 -0500

    some kind of stupid arithmetic error
    …for which I am compensating in a gross way

--- Source/Routines/GetMonsterColors.s
+++ Source/Routines/GetMonsterColors.s
@@ -6,18 +6,25 @@ GetMonsterColors:   .block
           lda #>MonsterColors
           sta Pointer + 1
 
-          lda CurrentMonsterNumber
-          asl a
-          asl a          
-          asl a
+          lda CurrentMonsterNumber ; 0 … $2c
+          asl a                 ; 0 … $58
+          asl a                 ; 0 … $b0
+          asl a                 ; 0 … $160
           bcc +
-          inc Pointer + 1
+          inc Pointer + 1       ; 0 … $60
 +
           adc #<MonsterColors
           bcc +
           inc Pointer + 1
 +
           sta Pointer
+
+          ;; unclear what I did wrong here.
+          lda #$20
+          and CurrentMonsterNumber
+          beq +
+          dec Pointer
++
 
           ldy # 0
           ldx # 8
