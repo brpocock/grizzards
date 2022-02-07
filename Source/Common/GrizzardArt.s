7062c050421e65cb71b8d6b20f275eece20e2aeb
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-07 18:12:49 -0500

    Update all Grizzards to final(?) art & colors
    Close #314.

--- Source/Common/GrizzardArt.s
+++ Source/Common/GrizzardArt.s
@@ -3,11 +3,11 @@
 
 GrizzardPictureSelect:
 
-          .byte 0, 3, 1, 8, 5, 2, 6, 7, 8, 9
+          .byte 0, 3, 1, 8, 5, 2, 6, 7, 0, 3
           ;; 10
-          .byte 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+          .byte 1, 8, 4, 9, 2, 5, 6, 7, 5, 7
           ;; 20
-          .byte 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+          .byte 4, 0, 3, 1, 8, 0, 3, 1, 8, 9
 
 GrizzardColor:
 
@@ -18,28 +18,28 @@ GrizzardColor:
           .colu COLBROWN, $4
           .colu COLTURQUOISE, $c
           .colu COLTEAL, $c
-          .colu COLGREEN, $c
-          .colu COLGREEN, $c
-          .colu COLGREEN, $c
+          .colu COLGREEN, $6
+          .colu COLSEAFOAM, $c
+          .colu COLGOLD, $6
           ;; 10
+          .colu COLBROWN, $c
           .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
-          .colu COLTURQUOISE, $c
+          .colu COLTEAL, $6
+          .colu COLYELLOW, $c
+          .colu COLBLUE, $6
+          .colu COLINDIGO, $6
+          .colu COLMAGENTA, $c
+          .colu COLORANGE, $c
+          .colu COLCYAN, $6
+          .colu COLGREEN, $c
           ;; 20
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
-          .colu COLINDIGO, $c
+          .colu COLBROWN, $c
+          .colu COLTEAL, $6
+          .colu COLRED, $c
+          .colu COLTURQUOISE, $c
+          .colu COLTEAL, $6
+          .colu COLSEAFOAM, $c
+          .colu COLGOLD, $c
+          .colu COLPURPLE, $6
+          .colu COLYELLOW, $c
+          .colu COLINDIGO, $8
