94246c72ca076bdb40179ddc614c38ebead69e41
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-15 17:29:52 -0500

    audited

--- Source/Routines/GrizzardMetamorphoseP.s
+++ Source/Routines/GrizzardMetamorphoseP.s
@@ -1,8 +1,9 @@
 ;;; Grizzards Source/Routines/GrizzardMetamorphoseP.s
 ;;; Copyright © 2022 Bruce-Robert Pocock
 
+          ;; If CurrentGrizzard can metamorphose, the Grizzard into which it can
+          ;; do will be returned in Y, else Y = 0
 GrizzardMetamorphoseP:    .block
-
           lda CurrentGrizzard
           ;; × 5
           asl a
@@ -18,3 +19,5 @@ GrizzardMetamorphoseP:    .block
           rts
 
           .bend
+
+;;; Audited 2022-02-15 BRPocock
