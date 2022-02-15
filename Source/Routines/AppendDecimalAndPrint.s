6ea5487d24ad1a9bf9a15b20fd5cb4003aa90ec2
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-15 00:23:16 -0500

    support for 3-digit numbers

--- Source/Routines/AppendDecimalAndPrint.s
+++ Source/Routines/AppendDecimalAndPrint.s
@@ -5,9 +5,30 @@ AppendDecimalAndPrint:        .block
 FromTemp:
           jsr CopyPointerText
 
+          ;; Doing +100/200 we want to adjust to make room
+          ;; in the case of "-##" we want "-###" to appear
+          lda Temp
+          cmp # 200
+          blt Done200
+          lda StringBuffer + 3
+          cmp # 39              ; "-"
+          bne +
+          sta StringBuffer + 2
++
+          lda # 2
+          sta StringBuffer + 3
+Done200
+          cmp # 100
+          blt Done100
+          lda StringBuffer + 3
+          cmp # 39              ; "-"
+          bne +
+          sta StringBuffer + 2
+          lda # 1
+          sta StringBuffer + 3
+Done100
+
           ;; based on  https://stackoverflow.com/questions/65432063/6502-assembly-binary-to-bcd-is-that-possible-on-x86
-
-
           ;; Not to self, consider also https://atariage.com/forums/topic/330847-binary-to-bcd/?do=findComment&comment=4999910 ?
 
 BINBCD8:
@@ -37,9 +58,9 @@ CNVBIT:
           sta StringBuffer + 5
 
           .if BANK == TextBank
-          jmp DecodeAndShowText ; tail call
+            jmp DecodeAndShowText ; tail call
           .else
-          .FarJMP TextBank, ServiceDecodeAndShowText ; also tail call
+            .FarJMP TextBank, ServiceDecodeAndShowText ; also tail call
           .fi
 
           .bend
