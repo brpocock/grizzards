531c73714838ba397e5599a4d557f840f72b1b70
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-15 23:29:54 -0500

    pencil in AtariAge cart save based on AtariVox

--- Source/Routines/AtariAgeSave-EEPROM-Driver.s
+++ Source/Routines/AtariAgeSave-EEPROM-Driver.s
@@ -2,44 +2,155 @@
 ;;;
 ;;; AtariAgeSave EEPROM Driver
 ;;;
-;;; COMING SOON from Fred Quimby
+;;; Based on AtariVoxEEPROMDriver
 ;;;
-;;; Adapted for Turbo Assembler by Bruce-Robert Pocock, 2022
+;;; Original by Alex Herbert, 2004
+;;;
+;;; Converted and adapted by Bruce-Robert Pocock, 2017, 2020-2022
 SaveGameSignatureString:
           .text SaveGameSignature
 
+          i2cControlPort = $fff0
+          i2cWritePort = $fff1
+          i2cReadPort = $fff2
+
+          i2cSDAMask = $04
+          i2cSCLMask = $08
+
+          SaveWritesPerScreen = $20
 ;;; 
+i2cSCL0:  .macro
+          lda # 0
+          sta i2cWritePort
+          .endm
 
-          ;; The AtariVox EEPROM driver is about 192 bytes ($c0)
-          ;; This fill maxxes out bank 0,1 for AA PAL/SECAM
-          ;; if we need more room things gonna get ugly 😎
-          .fill $b9, $ea
+i2cSCL1:  .macro
+          lda #i2cSCLMask
+          sta i2cWritePort
+          .endm
 
+i2cSDAIn: .macro
+          lda #i2cSCLMask
+          sta i2cControlPort
+          .endm
+
+i2cSDAOut: .macro
+          lda #i2cSCLMask | i2cSDAMask
+          sta i2cControlPort
+          .endm
+
+i2cReset: .macro
+          lda #0
+          sta i2cControlPort
+          .endm
+
+i2cStart: .macro
+          .i2cSCL1
+          .i2cSDAOut
+          .endm
+
+i2cStop:  .macro
+          .i2cSCL0
+          .i2cSDAOut
+          .i2cSCL1
+          .i2cReset
+          .endm
+
+i2cTxBit: .macro
+          .i2cSCL0
+          lda # 1
+          rol a
+          asl a
+          asl a
+          sta i2cControlPort ; SDA = !C
+          .i2cSCL1
+          .endm
+
+i2cTxACK: .macro
+          .i2cSCL0
+          .i2cSDAOut
+          .i2cSCL1
+          .endm
+
+i2cTxNAK: .macro
+          .i2cSCL0
+          .i2cSDAIn
+          .i2cSCL1
+          .endm
+
+i2cRxBit: .macro
+          .i2cSCL0
+          .i2cSDAIn
+          .i2cSCL1
+          lda i2cReadPort
+          lsr a
+          lsr a
+          lsr a ; C = SDA
+          .endm
+
+i2cRxACK: .macro
+          .i2cRxBit
+          .endm
+
+          EEPROMRead = %10100001
+          EEPROMWrite = %10100000
+;;; 
 i2cStartRead:
-          ;; TODO
+          clv               ; Use V to flag if previous byte needs ACK
+          .i2cStart
+          lda # EEPROMRead
+          bvc i2cTxByte
 
 i2cStartWrite:
-          ;; TODO
+          .i2cStart
+          lda # EEPROMWrite
 
 i2cTxByte:
-          ;; TODO
+          eor #$ff
+          sta Temp
 
-i2cRxByte:
-          ;; TODO
+          ldy # 8           ; loop (bit) counter
+i2cTxByteLoop:
+          asl Temp          ; next bit into C
+          .i2cTxBit
+          dey
+          bne i2cTxByteLoop
 
-i2cRxSkipACK:
-          ;; TODO
-
-i2cStopRead:
-          ;; TODO
-
-i2cStopWrite:
-          ;; TODO
+          .i2cRxACK         ; receive ACK bit
           rts
 
-i2cK:
+i2cRxByte:
+          ldy # 8           ; loop (bit) counter
+          bvc i2cRxSkipACK
+          .i2cTxACK
+
+i2cRxByteLoop:
+          .i2cRxBit
+          rol Temp          ; rotate C into scratchpad
+          dey
+          bne i2cRxByteLoop
+
+          lda Temp
+          rts
+
+i2cRxSkipACK:
+          bit VBit          ; set V - next byte/s require ACK
+VBit:
+          bvs i2cRxByteLoop
+
+i2cStopRead:
+          bvc i2cStopWrite
+          .i2cTxNAK
+
+i2cStopWrite:
+          .i2cStop
+          rts
+
+          ;; The following functions added by BRPocock, not found in the
+          ;; standard library code
+i2cK:                           ; K is "switch over to (you) sending" in Morse code
           jsr i2cTxByte
-i2cK2:
+i2cK2:                          ; switch without a final byte to send
           jsr i2cStopWrite
           jmp i2cStartRead      ; tail call
 
@@ -50,4 +161,4 @@ i2cWaitForAck:
           bcs -
           jmp i2cStopWrite      ; tail call
 
-          SaveWritesPerScreen = $20
+          .fill 46              ; In case this has to grow
