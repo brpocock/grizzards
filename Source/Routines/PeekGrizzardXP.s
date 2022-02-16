005dbb5a2e30987cc5c6675dab5561109822d576
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-02-16 13:09:00 -0500

    audited

--- Source/Routines/PeekGrizzardXP.s
+++ Source/Routines/PeekGrizzardXP.s
@@ -9,27 +9,34 @@ PeekGrizzardXP:       .block
 
           lda Temp
           jsr SetGrizzardAddress ; takes input in .A as well
-          jsr i2cStopWrite
-          jsr i2cStartRead
+
+          jsr i2cK2
 
           ;; Max HP, Attack, Defend, XP
           ldx # 4
 -
           jsr i2cRxByte
+
           dex
-          bne 0
+          bne -
+
           beq NoGrizzard        ; XP = 0 = no Grizzard
+
           cmp #$ff
           beq NoGrizzard        ; Garbage read = no Grizzard
 
           jsr i2cStopRead
+
           ;; Grizzard found!
           sec
           rts
 
 NoGrizzard:
           jsr i2cStopRead
+
           clc
           rts
 
           .bend
+
+;;; Audited 2022-02-16 BRPocock
