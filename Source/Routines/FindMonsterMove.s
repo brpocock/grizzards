a50bacf7420bd544fa9024ee1096078bac6c32dd
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-04-18 10:08:23 -0400

    audited

--- Source/Routines/FindMonsterMove.s
+++ Source/Routines/FindMonsterMove.s
@@ -1,21 +1,23 @@
 ;;; Grizzards Source/Routines/FindMonsterMove.s
 ;;; Copyright © 2021, 2022 Bruce-Robert Pocock
 
-FindMonsterMove:
+FindMonsterMove:    .block
+          ;; Y = move selection
+
+          .mvaw Pointer, MonsterMoves
           .mva Pointer + 1, #>MonsterMoves
 
-          clc
           ldx CurrentCombatEncounter
           lda EncounterMonster, x
           asl a
           asl a
-          adc #<MonsterMoves
-          bcc +
-          inc Pointer + 1
-+
-          sta Pointer
+          .Add16a Pointer
 
-          lax (Pointer), y      ; Y = MoveSelection
-          stx CombatMoveSelected
+          lda (Pointer), y
+          sta CombatMoveSelected
 
           rts
+
+          .bend
+
+;;; Audited 2022-04-18 BRPocock
