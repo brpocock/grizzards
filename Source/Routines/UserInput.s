c8a27ad76e7cede1b2d8fa57e791031fd037d8cf
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-07-15 13:06:25 -0400

    use .ceil notation not #ceil, for consistency

--- Source/Routines/UserInput.s
+++ Source/Routines/UserInput.s
@@ -150,7 +150,7 @@ FractionalMovement: .macro deltaVar, fractionVar, positionVar, pxPerSecond
 
 MoveMinus:
           sec
-          sbc #ceil(\pxPerSecond * $80)
+          sbc .ceil(\pxPerSecond * $80)
           sta \fractionVar
           bcs DoneMovement
 
@@ -161,7 +161,7 @@ MoveMinus:
 
 MovePlus:
           clc
-          adc #ceil(\pxPerSecond * $80)
+          adc .ceil(\pxPerSecond * $80)
           sta \fractionVar
           bcc DoneMovement
 
