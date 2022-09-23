4b823a8b27f206d2acf1a07293fc4aa3ab45e207
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-09-23 14:49:47 -0400

    rev from @batari

--- Source/Routines/AtariAgeSave-EEPROM-Driver.s
+++ Source/Routines/AtariAgeSave-EEPROM-Driver.s
@@ -14,6 +14,9 @@ SaveGameSignatureString:
           .text SaveGameSignature
 
           SaveWritesPerScreen = $20
+
+          i2cSDAMask = $04
+          i2cSCLMask = $08
 ;;; 
 i2cSCL0:  .macro
           nop $1ff0
@@ -23,45 +26,36 @@ i2cSCL1:  .macro
           nop $1ff1
           .endm
 
+i2cSDA0:  .macro
+          nop $1ff2
+          .endm
+
+i2cSDA1:  .macro
+          nop $1ff3
+          .endm
+
 i2cSDAIn: .macro
           nop $1ff3
           .endm
 
-i2cSDAOut: .macro
-          .endm
-
-i2cReset: .macro
-          nop $1ff0
-          nop $1ff3
-          .endm
-
-i2cStart: .macro
-          .i2cSCL1
-          .i2cSDAOut
-          .endm
-
-i2cStop:  .macro
+i2cStart: .macro ; SDA transitions 1 to 0 during SCL=1
           .i2cSCL0
-          .i2cSDAOut
+          .i2cSDA1
           .i2cSCL1
-          .i2cReset
+          .i2cSDA0
           .endm
 
-i2cTxACK: .macro
+i2cStop:  .macro ; SDA transitions 0 to 1 during SCL=1
           .i2cSCL0
-          .i2cSDAOut
+          .i2cSDA0
           .i2cSCL1
+          .i2cSDA1
           .endm
 
-i2cTxNAK: .macro
-          .i2cSCL0
-          .i2cSDAIn
-          .i2cSCL1
-          .endm
 
           EEPROMRead = %10100001
           EEPROMWrite = %10100000
-
+;;; 
 i2cStartRead:
           clv               ; Use V to flag if previous byte needs ACK
           .i2cStart
@@ -80,18 +74,17 @@ i2cTxByteLoop:
           asl Temp          ; next bit into C
           .i2cSCL0
           bcc Send0
-
-          nop $1ff3
-          bne Sent
-
+          .i2cSDA1
+          bcs Send1
 Send0:
-          nop $1ff2
-
-Sent:     
-          .i2cSCL1
+          .i2cSDA0
+Send1:
           dey
+          .i2cSCL1
           bne i2cTxByteLoop
-
+          .i2cSCL0
+          .i2cSDA1
+          .i2cSCL1 ; to enable read of ACK bit
           lda $1FF4
           lsr
           rts
@@ -100,13 +93,17 @@ i2cRxByte:
           ldy # 8           ; loop (bit) counter
           bvc i2cRxSkipACK
 
-          .i2cTxACK
+          .i2cSCL0 ; send ACK
+          .i2cSDA1
+          .i2cSDA0 ; Seems to want to send 1->0?
+          .i2cSCL1
 
+i2cRxByteLoopStart:
+          .i2cSDAIn
 i2cRxByteLoop:
           .i2cSCL0
-          .i2cSDAIn
           .i2cSCL1
-          lda $1FF4
+          lda $1FF4 ; get bit from EEPROM
           lsr
           rol Temp          ; rotate C into scratchpad
           dey
@@ -118,12 +115,15 @@ i2cRxByteLoop:
 i2cRxSkipACK:
           bit VBit          ; set V - next byte/s require ACK
 VBit:
-          bvs i2cRxByteLoop
+          bvs i2cRxByteLoopStart
 
 i2cStopRead:
           bvc i2cStopWrite
 
-          .i2cTxNAK
+          .i2cSCL0
+          .i2cSDA0 ; seems to go from 0->1??
+          .i2cSDA1
+          .i2cSCL1
 
 i2cStopWrite:
           .i2cStop
@@ -144,4 +144,3 @@ i2cWaitForAck:
 
           bcs i2cWaitForAck
           jmp i2cStopWrite      ; tail call
-
