33ae781a3480d4468e3505ddff2fcd1500ad5e73
Bruce-Robert Pocock <brpocock@star-hope.org>
2022-10-03 23:46:39 -0400

    use PeekGrizzard for returns from PeekGrizzardXP

--- Source/Routines/PeekGrizzardXP.s
+++ Source/Routines/PeekGrizzardXP.s
@@ -18,23 +18,12 @@ PeekGrizzardXP:       .block
           jsr i2cStartRead
 
           jsr i2cRxByte         ; Max HP
-          bne FoundGrizzard
+          bne PeekGrizzard.FoundGrizzard
 
           jsr i2cRxByte         ; Attack
           jsr i2cRxByte         ; Defend
           jsr i2cRxByte         ; XP
-          beq NoGrizzard
-
-FoundGrizzard:
-          jsr i2cStopRead
-
-          .mva Temp, #$80
-          rts
-
-NoGrizzard:
-          jsr i2cStopRead
-
-          .mva Temp, # 0
-          rts
+          beq PeekGrizzard.NoGrizzard
+          bne PeekGrizzard.FoundGrizzard
 
           .bend
